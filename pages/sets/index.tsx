94a8c5947cf68d2a51ac33f339bbfabd71381b25
Ursuss21 <robert.skrzypczak21@gmail.com>
2021-04-12 21:11:00 +0200

    Added Rebrickable API connection. Config changes.

--- pages/sets/index.tsx
+++ pages/sets/index.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import Link from "next/link";
+import { GetStaticProps } from "next";
 
-const SetsMainPage = () => {
+interface IProps {
+  count: number;
+  sets: [
+    {
+      set_num: string;
+      name: string;
+      year: number;
+      theme_id: number;
+      num_parts: number;
+      set_img_url: string;
+      set_url: string;
+    }
+  ];
+}
+
+const SetsMainPage: FunctionComponent<IProps> = ({ count, sets }) => {
+  console.log(count);
   return (
     <div>
       <h1>Sets Page</h1>
+      <div>
+        {sets.map((set) => {
+          return <div key={set.set_num}>{set.set_num}</div>;
+        })}
+      </div>
       <Link href="/">
         <a>Main Page</a>
       </Link>
@@ -13,3 +35,17 @@ const SetsMainPage = () => {
 };
 
 export default SetsMainPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch(
+    `${process.env.REBRICKABLE_URL}sets/?key=${process.env.REBRICKABLE_API_KEY}`
+  );
+  const data = await res.json();
+  console.log(data.results);
+  return {
+    props: {
+      sets: data.results,
+      count: data.count,
+    },
+  };
+};
