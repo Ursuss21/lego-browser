d0e74e19b1ebd13e8425c2ba4fc1b38226b38d70
Ursuss21 <robert.skrzypczak21@gmail.com>
2021-04-13 11:16:43 +0200

    Added static paths to sets.

--- pages/sets/[id].tsx
+++ pages/sets/[id].tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { GetStaticProps, GetStaticPaths } from "next";
 
 const Page = () => {
   const router = useRouter();
 
   const { id } = router.query;
 
-  return <div>Set {id}</div>;
+  if (router.isFallback) {
+    return <div>Loading</div>;
+  } else {
+    return <div>Set {id}</div>;
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const url = process.env.REBRICKABLE_URL;
+  const key = process.env.REBRICKABLE_API_KEY;
+
+  const res = await fetch(`${url}sets/?key=${key}`);
+  const data = await res.json();
+
+  const paths = data.results.map((set: any) => ({
+    params: {
+      id: set.set_num,
+    },
+  }));
+  return { paths, fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (typeof params !== "undefined") {
+    const url = process.env.REBRICKABLE_URL;
+    const key = process.env.REBRICKABLE_API_KEY;
+
+    const res = await fetch(`${url}sets/${params.id}/?key=${key}`);
+    const data = await res.json();
+
+    return {
+      props: {
+        sets: data,
+      },
+    };
+  }
+  return { props: {}, revalidate: 1 };
 };
 
 export default Page;
