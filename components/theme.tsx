088a5523a242012183cba152a772c451bddb005d
Ursuss21 <robert.skrzypczak21@gmail.com>
2021-05-03 14:04:11 +0200

    Fixed theme dropdown names.

--- components/theme.tsx
+++ components/theme.tsx
@@ -17,26 +17,79 @@ interface ITheme {
   name: string;
 }
 
-// const prepareThemeNames = (
-//   themes: [
-//     {
-//       id: number;
-//       parent_id: number;
-//       name: string;
-//     }
-//   ]
-// ) => {
-//   for (let theme of themes) {
-//     let temp: string = theme.name;
-//   }
-// };
+const prepareThemesNames = (
+  themes: [
+    {
+      id: number;
+      parent_id: number;
+      name: string;
+    }
+  ]
+) => {
+  const resultThemes: {
+    id: number;
+    parent_id: number;
+    name: string;
+  }[] = [];
+  for (const theme of themes) {
+    let temp: ITheme = theme;
+    let retName: string = temp.name;
+    while (temp.parent_id != null) {
+      const parent: ITheme | undefined = themes.find((t) => {
+        if (t.id === temp.parent_id) {
+          return true;
+        }
+      });
+      if (parent !== undefined) {
+        retName = `${parent.name} > ${retName}`;
+        temp = parent;
+      }
+    }
+    const resObj = { id: theme.id, parent_id: theme.parent_id, name: retName };
+    resultThemes.push(resObj);
+  }
+  return resultThemes;
+};
+
+const sortThemesByName = (
+  themes: {
+    id: number;
+    parent_id: number;
+    name: string;
+  }[]
+) => {
+  themes.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return themes;
+};
+
+const prepareThemesArray = (
+  themes: [
+    {
+      id: number;
+      parent_id: number;
+      name: string;
+    }
+  ]
+) => {
+  let resultThemes = prepareThemesNames(themes);
+  resultThemes = sortThemesByName(resultThemes);
+  return resultThemes;
+};
 
 const Theme: FunctionComponent<IProps> = ({ themes, themesCount }) => {
-  //prepareThemeNames(themes);
+  const preparedThemes = prepareThemesArray(themes);
   return (
     <div className="theme-dropdown">
       <select>
-        {themes.map((theme) => {
+        {preparedThemes.map((theme) => {
           return (
             <option key={theme.id} value={theme.id}>
               {theme.name}
